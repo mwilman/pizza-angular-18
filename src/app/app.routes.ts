585700b7ddcd87fe13e69626b935c1b16f6533e3
Maxim Wilman <mwilman@brockhaus-ag.de>
2024-07-18 12:41:37 +0200

    adding main components templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {PizzaMenuComponent} from "./pizza-menu/pizza-menu.component";
+import {CartComponent} from "./cart/cart.component";
+import {LandingPageComponent} from "./landing-page/landing-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'pizza', component: PizzaMenuComponent},
+  {path: 'home', component: LandingPageComponent},
+  // {path: 'pasta', component: PastaOverviewComponent},
+  // {path: 'schnitzel', component: SchnitzelOverviewComponent},
+  // {path: 'snacks', component: SnacksOverviewComponent},
+  // {path: 'all', component: AllOverviewComponent},
+  // {path: 'imprint', component: ImprintComponent},
+  // {path: 'contact', component: ContactComponent},
+  {path: 'cart', component: CartComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+];
